7eabea37a9b090b3c4237db15494831f5a6d74ea
nguyenlz <146945072+nguyenlz@users.noreply.github.com>
2023-12-20 19:59:56 +0700

    sửa giao diện

--- FastGuard/Controllers/AppRolesController.cs
+++ FastGuard/Controllers/AppRolesController.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FastGuard.Data;
+using System.Data;
 
 namespace FastGuard.Controllers
 {
     [Authorize(Roles="Admin")]
     public class AppRolesController : Controller
     {
-        private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly ApplicationDbContext _context;
+		private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AppRolesController(RoleManager<IdentityRole> roleManager)
+        public AppRolesController(RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
         {
             _roleManager = roleManager;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -35,6 +40,121 @@ namespace FastGuard.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+		public async Task<IActionResult> Details(string id)
+		{
+			if (id == null || _context.Roles == null)
+			{
+				return NotFound();
+			}
+
+			var appRole = await _context.Roles
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (appRole == null)
+			{
+				return NotFound();
+			}
+
+			return View(appRole);
+		}
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var appRole = await _context.Roles.FindAsync(id);
+            if (appRole == null)
+            {
+                return NotFound();
+            }
+            return View(appRole);
+        }
+
+        // POST: AppRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name")] IdentityRole appRole)
+        {
+            if (id != appRole.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var role = await _context.Roles.FindAsync(id);
+                    if (role == null)
+                    {
+                        return NotFound();
+                    }
+
+                    role.Name = appRole.Name; // Cập nhật tên vai trò
+
+                    _context.Roles.Update(role);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppRoleExists(appRole.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appRole);
+        }
+
+        // GET: AppRoles/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var appRole = await _context.Roles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appRole == null)
+            {
+                return NotFound();
+            }
+
+            return View(appRole);
+        }
+
+        // POST: AppRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Roles == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.AppRole'  is null.");
+            }
+            var appRole = await _context.Roles.FindAsync(id);
+            if (appRole != null)
+            {
+                _context.Roles.Remove(appRole);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppRoleExists(string id)
+        {
+            return (_context.Roles?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
